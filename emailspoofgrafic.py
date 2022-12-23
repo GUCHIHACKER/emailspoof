65fe7914fc1d4a78bcafa8fc0fee0fae4133a808
EL.Fenix <82448575+3LF3NIX@users.noreply.github.com>
2022-12-23 17:55:08 +0100

    Add files via upload

---
+++ emailspoofgrafic.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send', methods=['POST'])
+def send_email():
+    username = "HERE" # <--------- Put your sendinblue email HERE
+    password = "HERE" # <--------- Put your sendinblue master password HERE
+    target = request.form['target']
+    name = request.form['name']
+    sender = request.form['sender']
+    subject = request.form['subject']
+    message = request.form['message']
+
+    if name == '':
+        os.system('sendemail -xu \"'+username+'\" -xp \"'+password+'\" -f \"'+sender+'\" -t \"'+target+'\" -u \"'+subject+'\" -m \"'+message+'\" -s "smtp-relay.sendinblue.com:587" &>/dev/null')
+    else:
+        os.system('sendemail -xu \"'+username+'\" -xp \"'+password+'\" -f \"'+sender+'\" -t \"'+target+'\" -u \"'+subject+'\" -m \"'+message+'\" -s "smtp-relay.sendinblue.com:587" -o message-header="From: '+name+' <'+sender+'>" &>/dev/null')
+    return '<p>Email send</p><br><a href="/">Click to send another email</a>'
+
+if __name__ == '__main__':
+    app.run()
